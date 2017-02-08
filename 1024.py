8f5e8c22eb2aabebc07591666dc9c4beab41bcef
Bill <bill.chandos@gmail.com>
2017-02-08 05:56:25 -0800

    Initial commit

---
+++ 1024.py
@@ -0,0 +1,175 @@
+from random import randint, random
+game_board = [[None for cell in range(4)] for cell in range(4)]
+GAME_SCORE = 0
+
+def spawn_number():
+    # pick a random spot on the board
+    random_row = randint(0, 3)
+    random_col = randint(0, 3)
+    # determine if it's available for spawning
+    if not game_board[random_row][random_col]:
+        if random() > 0.8:
+            new_tile = 4
+        else:
+            new_tile = 2
+        game_board[random_row][random_col] = new_tile
+        return True
+    else:
+        return False
+
+def move_right():
+    """ Move all tiles to the right """
+    global GAME_SCORE
+    for row in game_board:
+        # Sort the row with None as 0 and all tiles as 1
+        # Sort is stable and so tiles stay in order
+        row.sort(key=lambda x: 0 if x is None else 1)
+        # Iterate backwards over list to find identical tiles and combine
+        # Insert blank tiles at beginning of list
+        for cell_index in range(3,0,-1):
+            if row[cell_index] and row[cell_index] == row[cell_index-1]:
+                # if the cell is not None and equal in value to the cell to its left
+                # combine the values
+                row[cell_index] *= 2
+                # add the combined value to the total score
+                GAME_SCORE += row[cell_index]
+                # remove the combined cell
+                row.pop(cell_index-1)
+                # insert a blank cell at the beginning of the row
+                row.insert(0, None)
+
+def move_left():
+    """ Move all tiles to the left """
+    global GAME_SCORE
+    for row in game_board:
+        # Sort the row with None as 0 and all tiles as 1, reversed
+        # Sort is stable and so tiles stay in order
+        row.sort(key=lambda x: 0 if x is None else 1, reverse=True)
+        # Iterate backwards over list to find identical tiles and combine
+        # Insert blank tiles at beginning of list
+        for cell_index in range(3):
+            if row[cell_index] and row[cell_index] == row[cell_index+1]:
+                # if the cell is not None and equal in value to the cell to its right
+                # combine the values
+                row[cell_index] *= 2
+                # add the combined value to the total score
+                GAME_SCORE += row[cell_index]
+                # remove the combined cell
+                row.pop(cell_index + 1)
+                # insert a blank cell at the end of the row
+                row.append(None)
+
+def move_down():
+    """ Move all tiles down """
+    global GAME_SCORE
+    for col_index in range(4):
+        vertical_row = []
+        for row_index in range(4):
+            # We convert each column to a list for ease of processing
+            vertical_row.append(game_board[row_index][col_index])
+        # Sort the row with None as 0 and all tiles as 1
+        # Sort is stable and so tiles stay in order
+        vertical_row.sort(key=lambda x: 0 if x is None else 1)
+        # Iterate backwards over list to find identical tiles and combine
+        # Insert blank tiles at beginning of list
+        for cell_index in range(3, 0, -1):
+            if vertical_row[cell_index] and vertical_row[cell_index] == vertical_row[cell_index - 1]:
+                # if the cell is not None and equal in value to the cell to its left
+                # combine the values
+                vertical_row[cell_index] *= 2
+                # add the combined value to the total score
+                GAME_SCORE += vertical_row[cell_index]
+                # remove the combined cell
+                vertical_row.pop(cell_index - 1)
+                # insert a blank cell at the beginning of the row
+                vertical_row.insert(0, None)
+        for row_index in range(4):
+            # We convert each processed list back into the column
+            game_board[row_index][col_index] = vertical_row[row_index]
+
+def move_up():
+    """ Move all tiles up """
+    global GAME_SCORE
+    for col_index in range(4):
+        vertical_row = []
+        for row_index in range(4):
+            # We convert each column to a list for ease of processing
+            vertical_row.append(game_board[row_index][col_index])
+        # Sort the row with None as 0 and all tiles as 1, reversed
+        # Sort is stable and so tiles stay in order
+            vertical_row.sort(key=lambda x: 0 if x is None else 1, reverse=True)
+        # Iterate backwards over list to find identical tiles and combine
+        # Insert blank tiles at beginning of list
+        for cell_index in range(3):
+            if vertical_row[cell_index] and vertical_row[cell_index] == vertical_row[cell_index + 1]:
+                # if the cell is not None and equal in value to the cell to its right
+                # combine the values
+                vertical_row[cell_index] *= 2
+                # add the combined value to the total score
+                GAME_SCORE += vertical_row[cell_index]
+                # remove the combined cell
+                vertical_row.pop(cell_index + 1)
+                # insert a blank cell at the end of the row
+                vertical_row.append(None)
+        for row_index in range(4):
+            # We convert each processed list back into the column
+            game_board[row_index][col_index] = vertical_row[row_index]
+
+def is_game_over():
+    """ Determines whether the game is over by first checking
+        if the board is full, then checking whether any moves
+        remain """
+    if not is_board_full():
+        # If there are empty cells, the game can continue
+        return False
+    # The game board is full and we must evaluate if there are moves
+    for row_index in range(4):
+        for col_index in range(4):
+            # Create list of all adjacent values
+            adjacents = []
+            if row_index-1 > 0:
+                adjacents.append(game_board[row_index-1][col_index])
+            if row_index+1 < 4:
+                adjacents.append(game_board[row_index+1][col_index])
+            if col_index-1 > 0:
+                adjacents.append(game_board[row_index][col_index-1])
+            if col_index+1 < 4:
+                adjacents.append(game_board[row_index][col_index+1])
+            # Test if tile value is in an adjacent cell, meaning play can continue
+            if game_board[row_index][col_index] in adjacents:
+                return False
+    return True
+
+def is_board_full():
+    """ Checks each row for empty cells """
+    for row in game_board:
+        if None in row:
+            return False
+    return True
+
+def main():
+    # establish first game pieces
+    spawn_number()
+
+    while not is_game_over():
+        if not is_board_full():
+            # As long a space is available, we want to add a piece
+            while not spawn_number(): pass
+        # Randomly select a move
+        random_move = randint(1, 4)
+        if random_move == 1:
+             move_up()
+        elif random_move == 2:
+             move_right()
+        elif random_move == 3:
+             move_down()
+        elif random_move == 4:
+             move_left()
+
+    for row in game_board:
+        print(*row, sep='\t')
+    print(GAME_SCORE)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
