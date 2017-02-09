d656ef157fe4f91dbbbd076c847bd217edb909d1
bchandos <bill.chandos@gmail.com>
2017-02-09 14:03:13 -0800

    Commented, and added game over dialog.

--- ten_twentyfour_gui.py
+++ ten_twentyfour_gui.py
@@ -1,33 +1,45 @@
 import tkinter as tk
+from tkinter import messagebox
 import ten_twentyfour as ten24
-import math
+from math import floor
 
 class GameGUI(tk.Frame):
-
+    """ Creates 1024 game UI and define keybindings
+        Game logic is handled in ten_twentyfour.py
+        Extends the tkinter Frame object """
     def __init__(self, master=None):
+        # initialize the tk.Frame object with same root
         super().__init__(master)
         ten24.game_board_setup()
+        # one-dimensional list containing tk.Lable widgets
         self.cells = []
+        self.score_label = None
         self.grid()
         self.master.bind('<Left>', self.left_key)
         self.master.bind('<Right>', self.right_key)
         self.master.bind('<Up>', self.up_key)
         self.master.bind('<Down>', self.down_key)
+        self.master.bind('<Escape>', self.quit)
         self.setup()
         self.draw()
 
     def setup(self):
-        self.score_label = tk.Label(self.master, text='Score: {}'.format(ten24.GAME_SCORE), width=40, height=5, relief='raised')
-        self.score_label.grid(row=5, columnspan = 4)
+        # Setup the window by creating a 4x4 grid, and adding each tile as tk.Label
+        # to list, while populating with initial values of game_board.
         for row_index, row in enumerate(ten24.game_board):
             for cell_index, cell in enumerate(row):
                 c = tk.Label(self.master, text=cell, width=10, height=5, relief='groove')
                 c.grid(row=row_index, column=cell_index)
                 self.cells.append(c)
+        self.score_label = tk.Label(self.master, text='Score: {}'.format(ten24.GAME_SCORE),
+                                    width=40, height=5, relief='raised')
+        self.score_label.grid(row=5, columnspan=4)
 
     def draw(self):
+        # Main drawing loop. Iterates over list of labels,
+        # clears current values, looks up new value in game_board
         for cell_index, cell in enumerate(self.cells):
-            row_index = int(math.floor(cell_index/4))
+            row_index = int(floor(cell_index/4))
             col_index = int(cell_index - (row_index * 4))
             cell.configure(text='')
             cell.configure(text=ten24.game_board[row_index][col_index])
@@ -35,6 +47,8 @@ class GameGUI(tk.Frame):
         if not ten24.GAME_OVER:
             self.after(100, self.draw)
         else:
+            messagebox.showinfo('Game Over!', 'Your game has ended and you are something of a failure. '
+                                                   f'Your final score was {ten24.GAME_SCORE}!')
             self.quit()
 
     def left_key(self, event):
@@ -50,6 +64,7 @@ class GameGUI(tk.Frame):
         ten24.play_game('down')
 
 root = tk.Tk()
+root.title('Totally Not 2048')
 app = GameGUI(master=root)
 app.mainloop()
 
