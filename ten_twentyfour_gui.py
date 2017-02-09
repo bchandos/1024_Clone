827fc1c065f7e2a9567f5efc1aa7922ec6de27d3
bchandos <bill.chandos@gmail.com>
2017-02-08 16:34:20 -0800

    More futzing around - no closer to working

--- ten_twentyfour_gui.py
+++ ten_twentyfour_gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import ten_twentyfour as ten24
 
+
 class Game(tk.Frame):
 
     def __init__(self, master=None):
@@ -8,18 +9,20 @@ class Game(tk.Frame):
         self.grid()
         self.bind('<Left>', self.leftKey)
         self.bind('<Right>', self.rightKey)
+        self.bind('<Up>', self.upKey)
+        self.bind('<Down>', self.downKey)
+        self.bind('<Button-1>', self.callback)
         self.draw()
 
     def draw(self):
-        # self.new_game = tk.Button(self, text='New Game', command=ten24.new_game())
-        # self.new_game.pack(side='top')
         for row_index, row in enumerate(ten24.game_board):
             for cell_index, cell in enumerate(row):
                 tk.Label(root, text=cell, width=10, height=5, relief='groove').grid(row=row_index, column=cell_index)
-        # self.after(10, self.draw)
+        self.master.after(100, self.draw)
 
-        # self.quit = tk.Button(self, text='QUIT', command=root.destroy)
-        # self.quit.pack(side='bottom')
+    def callback(self, event):
+        self.focus_set()
+        print("clicked at", event.x, event.y)
 
     def leftKey(event):
         ten24.move_left()
@@ -27,6 +30,13 @@ class Game(tk.Frame):
     def rightKey(event):
         ten24.move_right()
 
+    def upKey(event):
+        ten24.move_up()
+
+    def downKey(event):
+        ten24.move_down()
+
 root = tk.Tk()
 app = Game(master=root)
+
 app.mainloop()
