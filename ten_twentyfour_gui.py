89cf48783607a47e703a59b370fde5b5f77ea799
Bill <bill.chandos@gmail.com>
2017-02-08 22:25:16 -0800

    minor updates

--- ten_twentyfour_gui.py
+++ ten_twentyfour_gui.py
@@ -7,33 +7,37 @@ class Game(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.grid()
-        self.master.bind('<Left>', self.leftKey)
-        self.master.bind('<Right>', self.rightKey)
-        self.master.bind('<Up>', self.upKey)
-        self.master.bind('<Down>', self.downKey)
+        self.master.bind('<Left>', self.left_key)
+        self.master.bind('<Right>', self.right_key)
+        self.master.bind('<Up>', self.up_key)
+        self.master.bind('<Down>', self.down_key)
         self.draw()
 
     def draw(self):
         for row_index, row in enumerate(ten24.game_board):
             for cell_index, cell in enumerate(row):
-                tk.Label(root, text=cell, width=10, height=5, relief='groove').grid(row=row_index, column=cell_index)
-        self.master.after(100, self.draw)
+                c = tk.Label(root, text=cell, width=10, height=5, relief='groove').grid(row=row_index, column=cell_index)
+        self.after(100, self.draw)
 
-    def leftKey(self, event):
+    def left_key(self, event):
         ten24.move_left()
-        ten24.spawn_number()
+        while not ten24.spawn_number():
+            pass
 
-    def rightKey(self, event):
+    def right_key(self, event):
         ten24.move_right()
-        ten24.spawn_number()
+        while not ten24.spawn_number():
+            pass
 
-    def upKey(self, event):
+    def up_key(self, event):
         ten24.move_up()
-        ten24.spawn_number()
+        while not ten24.spawn_number():
+            pass
 
-    def downKey(self, event):
+    def down_key(self, event):
         ten24.move_down()
-        ten24.spawn_number()
+        while not ten24.spawn_number():
+            pass
 
 root = tk.Tk()
 app = Game(master=root)
